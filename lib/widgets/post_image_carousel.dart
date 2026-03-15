f01003675507e52b1c6b58ebd7657802453720b9
hariomsonihs <hariomsonihs@gmail.com>
2026-03-16 01:07:08 +0530

    fix: use photo_view for reliable pinch-to-zoom on post images

--- lib/widgets/post_image_carousel.dart
+++ lib/widgets/post_image_carousel.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
 
 class PostImageCarousel extends StatefulWidget {
   const PostImageCarousel({super.key, required this.imageUrls});
@@ -38,10 +39,37 @@ class _PostImageCarouselState extends State<PostImageCarousel> {
             controller: _pageController,
             itemCount: widget.imageUrls.length,
             onPageChanged: (i) => setState(() => _currentPage = i),
-            itemBuilder: (_, index) => _ZoomableImage(
-              imageUrl: widget.imageUrls[index],
-              size: width,
-            ),
+            itemBuilder: (_, index) {
+              return PhotoView(
+                imageProvider: CachedNetworkImageProvider(
+                  widget.imageUrls[index],
+                ),
+                minScale: PhotoViewComputedScale.contained,
+                maxScale: PhotoViewComputedScale.covered * 4,
+                initialScale: PhotoViewComputedScale.covered,
+                backgroundDecoration: const BoxDecoration(
+                  color: Colors.white,
+                ),
+                loadingBuilder: (_, __) => Container(
+                  color: const Color(0xFFF4F4F4),
+                  alignment: Alignment.center,
+                  child: const SizedBox(
+                    width: 24,
+                    height: 24,
+                    child: CircularProgressIndicator(strokeWidth: 2),
+                  ),
+                ),
+                errorBuilder: (_, __, ___) => Container(
+                  color: const Color(0xFFF0F0F0),
+                  alignment: Alignment.center,
+                  child: const Icon(
+                    Icons.broken_image_outlined,
+                    color: Colors.black38,
+                    size: 34,
+                  ),
+                ),
+              );
+            },
           ),
           if (widget.imageUrls.length > 1)
             Positioned(
@@ -68,98 +96,6 @@ class _PostImageCarouselState extends State<PostImageCarousel> {
               ),
             ),
         ],
-      ),
-    );
-  }
-}
-
-class _ZoomableImage extends StatefulWidget {
-  const _ZoomableImage({required this.imageUrl, required this.size});
-
-  final String imageUrl;
-  final double size;
-
-  @override
-  State<_ZoomableImage> createState() => _ZoomableImageState();
-}
-
-class _ZoomableImageState extends State<_ZoomableImage> {
-  double _scale = 1.0;
-  double _baseScale = 1.0;
-  Offset _offset = Offset.zero;
-  Offset _normalizedOffset = Offset.zero;
-
-  void _onScaleStart(ScaleStartDetails details) {
-    _baseScale = _scale;
-    _normalizedOffset = (_offset - details.focalPoint) / _scale;
-  }
-
-  void _onScaleUpdate(ScaleUpdateDetails details) {
-    if (details.pointerCount < 2) return;
-    setState(() {
-      _scale = (_baseScale * details.scale).clamp(1.0, 4.0);
-      _offset = details.focalPoint + _normalizedOffset * _scale;
-    });
-  }
-
-  void _onScaleEnd(ScaleEndDetails details) {
-    if (_scale <= 1.05) {
-      setState(() {
-        _scale = 1.0;
-        _offset = Offset.zero;
-      });
-    }
-  }
-
-  void _onDoubleTap() {
-    setState(() {
-      if (_scale > 1.0) {
-        _scale = 1.0;
-        _offset = Offset.zero;
-      } else {
-        _scale = 2.5;
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onScaleStart: _onScaleStart,
-      onScaleUpdate: _onScaleUpdate,
-      onScaleEnd: _onScaleEnd,
-      onDoubleTap: _onDoubleTap,
-      child: ClipRect(
-        child: Transform(
-          transform: Matrix4.identity()
-            ..translateByDouble(_offset.dx, _offset.dy, 0, 1)
-            ..scaleByDouble(_scale, _scale, 1, 1),
-          alignment: Alignment.center,
-          child: CachedNetworkImage(
-            imageUrl: widget.imageUrl,
-            fit: BoxFit.cover,
-            width: widget.size,
-            height: widget.size,
-            placeholder: (_, __) => Container(
-              color: const Color(0xFFF4F4F4),
-              alignment: Alignment.center,
-              child: const SizedBox(
-                width: 24,
-                height: 24,
-                child: CircularProgressIndicator(strokeWidth: 2),
-              ),
-            ),
-            errorWidget: (_, __, ___) => Container(
-              color: const Color(0xFFF0F0F0),
-              alignment: Alignment.center,
-              child: const Icon(
-                Icons.broken_image_outlined,
-                color: Colors.black38,
-                size: 34,
-              ),
-            ),
-          ),
-        ),
       ),
     );
   }
